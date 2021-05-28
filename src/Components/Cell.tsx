c6ca838f5a26d3bf289606f2d7ad9857bc05a751
dtr-beast <55450440+dtr-beast@users.noreply.github.com>
2021-05-28 15:23:55 +0530

    Cell and Maze

--- src/Components/Cell.tsx
+++ src/Components/Cell.tsx
@@ -4,7 +4,7 @@ import {Box} from "@material-ui/core";
 function Cell(props: {color?: string, img?: string}) {
     return (
         <Box bgcolor={props.color} border={"solid 1px black"} height={60} width={60}>
-            <img src={props.img} width={60} height={60}/>
+            <img src={props.img} width={60} height={60} alt={""}/>
         </Box>
     )
 }
--- src/Components/Maze.tsx
+++ src/Components/Maze.tsx
@@ -1,32 +1,30 @@
-import React, {ReactComponentElement} from 'react'
+import React from 'react'
 import Cell from "./Cell";
 import {Grid} from "@material-ui/core";
 import {v1 as uuidv1} from 'uuid';
 import maze from "../mazeCode";
 
-function calculatePaths() {
-}
+// function calculatePaths() {
+// }
 
 function Maze() {
-    console.log(maze)
-    console.log(typeof Grid)
-    console.log(typeof Cell)
-    const colors = ['red', 'white']
+    const colors = ['white', 'red']
     const mazeList = []
-    for (let i = 0; i < 4; i++) {
+
+    for (let i = 0; i < maze.length; i++) {
         const cellList = []
-        for (let j = 0; j < 4; j++) {
-            const number = Math.floor(Math.random() * colors.length);
-            cellList.push(<Cell key={uuidv1()} color={colors[number]}/>)
+        for (let j = 0; j < maze[i].length; j++) {
+            if (i === 0 && j === 0) {
+                cellList.push(<Cell key={uuidv1()} img={'covid.jpg'}/>)
+            } else if (i === 3 && j === 3) {
+                cellList.push(<Cell key={uuidv1()} img={'human.png'}/>)
+            } else {
+                cellList.push(<Cell key={uuidv1()} color={colors[maze[i][j]]}/>)
+            }
         }
         mazeList.push(<Grid key={uuidv1()} container item xs={12}>{cellList}</Grid>)
     }
-    console.log(mazeList)
-    // @ts-ignore
-    // mazeList[0][0] = <Cell key={uuidv1()} img={'covid.jpg'}/>
-    // @ts-ignore
-    // mazeList[3][3] = <Cell key={uuidv1()} img={'human.png'}/>
-    // mazeList[0][0] = <Cell key={uuidv1()} img={'covid.jpg'}/>
+
     return (
         <div>
             {mazeList}
